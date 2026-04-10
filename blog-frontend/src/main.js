71c35ac6b8a0188b5490bc5298b83c85882cd1fe
mzz555 <zhi75426@gmail>
2026-04-10 15:33:00 +0800

    初始化：Vue.js 前端项目骨架（Vite + Element Plus + Pinia）

---
+++ blog-frontend/src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { permissionDirective } from './directives/permission'
+
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(createPinia())
+app.use(router)
+app.directive('permission', permissionDirective)
+
+app.mount('#app')
