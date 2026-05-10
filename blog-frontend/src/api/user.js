753b9800f5f4ca8e8de6aef518c4afec6c59b49b
憨憨 <12290635+mzz55@user.noreply.gitee.com>
2026-05-10 18:46:19 +0800

    feat(frontend): api/user.js 新增 createUser 函数

--- blog-frontend/src/api/user.js
+++ blog-frontend/src/api/user.js
@@ -3,3 +3,4 @@ import request from './request'
 export const getUsers = (params) => request.get('/admin/users', { params })
 export const updateUserStatus = (id, status) => request.put(`/admin/users/${id}/status`, null, { params: { status } })
 export const updateUser = (id, data) => request.put(`/admin/users/${id}`, data)
+export const createUser = (data) => request.post('/admin/users', data)
